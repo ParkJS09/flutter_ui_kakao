b2cde4e4b73f63f17af66b7d5f85a0fe4d7634c3
JS <python2_7@naver.com>
2021-08-18 22:20:57 +0900

    210818_first_init 1. 기본 뼈대 구축

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_kakao/screens/main_scree.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          elevation: 0.0,
+          backgroundColor: Colors.white,
+          textTheme: TextTheme(
+            headline6: TextStyle(color: Colors.black, fontSize: 24)
+          ),
+          iconTheme: IconThemeData(
+            color: Colors.black
+          )
+        ),
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen()
+    );
+  }
+}
+
